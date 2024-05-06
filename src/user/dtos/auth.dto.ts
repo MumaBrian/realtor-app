03c18a102bc1459d97dc3440c16e3a0972eeedaf
Muma Brian <muma.neoworlder@gmail.com>
2024-05-06 12:17:11 +0100

    added sign in and sign up functionalities

---
+++ src/user/dtos/auth.dto.ts
@@ -0,0 +1,33 @@
+import {
+	IsString,
+	IsNotEmpty,
+	IsEmail,
+	MinLength,
+	Matches,
+} from 'class-validator';
+
+export class SignupDto {
+	@IsString()
+	@IsNotEmpty()
+	name: string;
+
+	@Matches(/^(\+\d{1,2}\s)?\(?\d+\)?[\s.-]?\d+[\s.-]?\d+$/, {
+		message: 'phone must be a valid phone number',
+	})
+	phone: string;
+
+	@IsEmail()
+	email: string;
+
+	@MinLength(5)
+	@IsString()
+	password: string;
+}
+
+export class SigninDto {
+	@IsEmail()
+	email: string;
+
+	@IsString()
+	password: string;
+}
